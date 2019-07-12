2bbaf454a21fbb271af4c4db4cfcf7e38d922283
arstrel <arstrel@gmail.com>
2019-07-12 18:23:42 -0400

    Basic views and GET routes added

---
+++ models/Setup.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const setupSchema = new Schema ({
+eventTipPercentage: Number,
+tipoutPercentage: Number,
+tipoutfoodRunnerPercentage: Number
+}, {
+  timestamps: {
+    createdAt: "created_at",
+    updatedAt: "updated_at"
+  }
+})
+
+const Setup = mongoose.model('Setup', setupSchema);
+
+module.exports = Setup;
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,24 @@ router.get('/', (req, res, next) => {
   res.render('index');
 });
 
+router.get('/profile', (req, res, next) => {
+  res.render('profile')
+})
+router.get('/signin', (req, res, next) => {
+  res.render('signin')
+})
+router.get('/login', (req, res, next) => {
+  res.render('login')
+})
+router.get('/calculator', (req, res, next) => {
+  res.render('calculator')
+})
+router.get('/report/edit', (req, res, next) => {
+  res.render('editReport')
+})
+router.get('/profile/edit', (req, res, next) => {
+  res.render('editProfile')
+})
+
+
 module.exports = router;
